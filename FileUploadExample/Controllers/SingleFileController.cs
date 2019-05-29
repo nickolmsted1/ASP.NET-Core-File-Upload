b6f306535f3c2c38ef0d69dffafe8e20d4b8c8e0
nick olmsted <go_go_loss@hotmail.com>
2019-05-29 15:02:51 -0700

    added ability to save image to root folder. also added ImageSharp api to resize image in future

--- FileUploadExample/Controllers/SingleFileController.cs
+++ FileUploadExample/Controllers/SingleFileController.cs
@@ -6,18 +6,26 @@ using FileUploadExample.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FileUploadExample.Controllers
 {
     public class SingleFileController : Controller
     {
+        private readonly IHostingEnvironment _env;
+
+        public SingleFileController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(Food item)
+        public async Task<IActionResult> Index(Food item)
         {
             //Either use a viewmodel or remove photoURL. URL will
             //be generated programmatically
@@ -30,17 +38,30 @@ namespace FileUploadExample.Controllers
                 //Check file extension is a photo
                 string extension = 
                        Path.GetExtension(photo.FileName);
-                if(extension == ".png" || extension == ".jpg")
+                if (extension == ".png" || extension == ".jpg")
                 {
 
+
+                    //reduce photo size
+                    // Use ImagSharp to resize image
+
+                    //generate unique name to retrieve later
+                    string newFileName = Guid.NewGuid().ToString();
+
+                    //store photo on file system and reference in DB
+                    if (photo.Length > 0) // Ensure file is not empty
+                    {
+                        string filePath = Path.Combine(_env.WebRootPath, "images", newFileName + extension);
+
+                        // save location to database(in URL format)
+                        item.PhotoUrl = "images/" + newFileName + extension;
+                        // write file to file system
+                        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                        {
+                            await photo.CopyToAsync(fs);
+                        }
+                    }
                 }
-                //reduce photo size
-
-                //generate unique name to retrieve later
-                string newFileName = Guid.NewGuid().ToString();
-
-                //store photo on file system and reference in DB
-                return View();
             }
 
             return View();
