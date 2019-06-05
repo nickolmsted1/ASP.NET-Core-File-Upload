553c6917b418dc47e9db7560496a8bf3fe584829
Nicholas Olmsted <37194521+nickolmsted1@users.noreply.github.com>
2019-06-05 11:33:29 -0700

    added file helper class to make code easier to read and less compilcated methods

---
+++ FileUploadExample/Models/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadExample.Models
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// save file to file system
+        /// </summary>
+        /// <param name="file">File to be saved</param>
+        /// <param name="name">The unique file name to save the file as</param>
+        /// <param name="path">The absolute path to save this file</param>
+        /// <exception cref="ArgumentNullException">Thrown when file name is null</exception>"
+        /// <returns></returns>
+        public static async Task SaveFile(IFormFile file, string name, string path)
+        {
+            if (name is null)
+                throw new ArgumentNullException($"{nameof(name)} cannot be null.")
+        }
+    }
+}
